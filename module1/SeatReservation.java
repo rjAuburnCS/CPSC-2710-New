cffab773d79b197c80615275fe8801c944fd5492
rjAuburnCS <basque_take_11@icloud.com>
2025-01-21 15:44:28 -0600

    Add getters, setters, and toString method

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -8,6 +8,27 @@ public class SeatReservation {
 
 
 
+public String getFlightDesignator() { return flightDesignator; }
+public void setFlightDesignator(String fd) { this.flightDesignator = fd; }
+
+public java.time.LocalDate getFlightDate() { return flightDate; }
+public void setFlightDate(java.time.LocalDate date) { this.flightDate = date; }
+
+public String getFirstName() { return firstName; }
+public void setFirstName(String fn) { this.firstName = fn; }
+
+public String getLastName() { return lastName; }
+public void setLastName(String ln) { this.lastName = ln; }
+
+@Override
+public String toString() {
+    return "SeatReservation{" +
+           "flightDesignator=" + (flightDesignator == null ? "null" : flightDesignator) + 
+           ", flightDate=" + (flightDate == null ? "null" : flightDate) + 
+           ", firstName=" + (firstName == null ? "null" : firstName) + 
+           ", lastName=" + (lastName == null ? "null" : lastName) + 
+           "}";
+}
 
 
 
