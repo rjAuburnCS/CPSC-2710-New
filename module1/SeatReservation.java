d4109668cffe66d9abd4e6702b3231cc22e65b14
rjAuburnCS <basque_take_11@icloud.com>
2025-01-22 14:49:47 -0600

    Fix all tests

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,43 +1,41 @@
 
 public class SeatReservation {
 
-	private String flightDesignator;
-	private java.time.LocalDate flightDate;
-	private String firstName;
-	private String lastName;
+   private String flightDesignator;
+   private java.time.LocalDate flightDate;
+   private String firstName;
+   private String lastName;
 
 
 
-public String getFlightDesignator() { return flightDesignator; }
-public void setFlightDesignator(String fd) { 
+   public String getFlightDesignator() { 
+      return flightDesignator; }
+   public void setFlightDesignator(String fd) {
+      if (fd == null || fd.length() < 4 || fd.length() > 6) {
+         throw new IllegalArgumentException("Flight Designator must be 4 - 6 characters long and not null.");
+      }
+      this.flightDesignator = fd;
+   }   
+   public java.time.LocalDate getFlightDate() { 
+      return flightDate; }
+   public void setFlightDate(java.time.LocalDate date) { this.flightDate = date; }
 
-	if (fd.length() < 4 || fd.length() > 6){
-		
-		throw new IllegalArgumentException("Flight Designator must be 4 - 6 char long.");
-	}
+   public String getFirstName() { 
+      return firstName; }
+   public void setFirstName(String fn) { this.firstName = fn; }
 
-this.flightDesignator = fd;
+   public String getLastName() { 
+      return lastName; }
+   public void setLastName(String ln) { this.lastName = ln; }
 
- }
+   @Override
+   public String toString() {
+      return "SeatReservation{" +
+            "flightDesignator=" + (flightDesignator == null ? "null" : flightDesignator) +
+            ",flightDate=" + (flightDate == null ? "null" : flightDate) +
+            ",firstName=" + (firstName == null ? "null" : firstName) +
+            ",lastName=" + (lastName == null ? "null" : lastName) +
+            "}";
+   }
 
-public java.time.LocalDate getFlightDate() { return flightDate; }
-public void setFlightDate(java.time.LocalDate date) { this.flightDate = date; }
-
-public String getFirstName() { return firstName; }
-public void setFirstName(String fn) { this.firstName = fn; }
-
-public String getLastName() { return lastName; }
-public void setLastName(String ln) { this.lastName = ln; }
-
-@Override
-public String toString() {
-    return "SeatReservation{" +
-           "flightDesignator=" + (flightDesignator == null ? "null" : flightDesignator) + 
-           ", flightDate=" + (flightDate == null ? "null" : flightDate) + 
-           ", firstName=" + (firstName == null ? "null" : firstName) + 
-           ", lastName=" + (lastName == null ? "null" : lastName) + 
-           "}";
 }
-
-}
-
