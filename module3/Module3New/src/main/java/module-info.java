3adb8341c11290a595859c274a5a9d97b8bc113c
rjAuburnCS <basque_take_11@icloud.com>
2025-02-11 11:41:14 -0600

    git issues

--- module3/Module3New/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.au.cpsc.module3new {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.au.cpsc.module3new to javafx.fxml;
-    exports edu.au.cpsc.module3new;
-}
